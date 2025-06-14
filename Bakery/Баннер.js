1defffdc9ff071a2f6413d8ab1bd4598e2218bcb
DenisElin2005 <elin_denis@inbox.ru>
2025-06-15 00:26:08 +0300

    Delete Bakery directory

--- Bakery/Баннер.js
+++
@@ -1,5 +0,0 @@
-<section class="banner">
-    <img src="banner.jpg" alt="Свежая выпечка">
-    <h1>Сладкие мечты становятся реальностью!</h1>
-    <a href="#menu" class="btn">Посмотреть меню</a>
-</section>
